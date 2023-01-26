b24b3f9ac56badba70e0b0f0b4eb4636536082e4
rosarie <rosariosanchezsampietro@gmail.com>
2023-01-26 08:14:45 -0300

    api connected and random function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const max = 122;
+const min = 1;
+const random = () => Math.floor(Math.random() * (max - min) + min);
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4' 
+
+    const image = document.createElement('img');
+    image.className = 'mx-auto';
+    image.width = '320';
+    image.src = `https://randomfox.ca/images/${random()}.jpg`;  
+
+    container.appendChild(image);
+    return container;
+};
+
+
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+};
+
+addButton.addEventListener("click", addImage);
+
+
